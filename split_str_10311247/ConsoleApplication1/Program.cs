490529fa52d41fbc1d2bd3ef14d772c576d7a326
Mean23 <chsh910174@gmail.com>
2017-03-05 23:07:55 +0900

    Lab2_HW

---
+++ split_str_10311247/ConsoleApplication1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int millisecondsTimeout = 30000;
+            using (StreamReader sr = new StreamReader(@"..\..\..\..\Data\Input.txt"))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    foreach (var ch in line)
+                    {
+                        switch (ch)
+                        {
+                            case '\u0021':
+                                Console.Write("!\n");
+                                break;
+                            case '\u002E':
+                                Console.Write(".\n");
+                                break;
+                            case '\u003F':
+                                Console.Write("?\n");
+                                break;
+                            default:
+                                Console.Write(ch);
+                                break;
+                        }
+                    }
+                }
+                System.Threading.Thread.Sleep(millisecondsTimeout);
+            }
+        }
+    }
+}
